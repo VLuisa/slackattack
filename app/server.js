43d3fa7c054532225fc7367001c2c8a64ce05981
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2016-07-17 21:43:58 -0400

    fixed heroku app crashing

--- app/server.js
+++ app/server.js
@@ -35,87 +35,93 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 
-controller.hears(['hungry', 'food'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-  function askIfHungry(response, convo) {
-    convo.ask('Are you hungry?', [
-      {
-        pattern: bot.utterances.yes,
-        callback: () => {
-          convo.say('I can help with that!');
-          askFoodType(response, convo);
-          convo.next();
-        },
-      },
-      {
-        pattern: bot.utterances.no,
-        callback: () => {
-          convo.say('Ok maybe later then.');
-          convo.next();
-        },
-      },
-      {
-        default: true,
-        callback: () => {
-          convo.repeat();
-          convo.next();
-        },
-      },
-    ]);
-  }
-
-  function askFoodType(response, convo) {
-    convo.ask('What type of food are you in the mood for?', (food) => {
-      convo.say('Okay');
-      askLocation(food, convo);
+// Adapted from https://github.com/olalonde/node-yelp
+// See http://www.yelp.com/developers/documentation/v2/search_api
+function searchYelp(food, location, convo) {
+  yelp.search({ term: food, location, limit: 2, sort: 2 })
+  .then((data) => {
+    data.businesses.forEach(business => {
+      const attachment = {
+        attachments: [
+          {
+            title: business.name,
+            title_link: business.url,
+            text: `${business.snippet_text}`,
+            image_url: `${business.image_url}`,
+            color: '#ff9910',
+          },
+        ],
+      };
+      convo.say(attachment);
       convo.next();
     });
-  }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
 
-  function askLocation(food, convo) {
-    convo.ask('Where are you?', (location) => {
-      convo.say(`So you want ${food.text} near ${location.text}`);
-      yelp.search({ term: `${food.text}`, location: `${location.text}`, limit: 2, sort: 2 }) // sort by highest rating, limit 3 searches
-      // yelp.search({ term: 'pizza', location: 'Chicago', limit: 3, sort: 2 }) // sort by highest rating, limit 3 searches
+function foodSearch(food, convo) {
+  convo.ask('Where are you?', (location) => {
+    // convo.say('Ok I\'ll search yelp for: ' + response.text);
+    const yelpResult = searchYelp(food.text, location.text, convo);
+    const yelpResponse = `Here's what I found: ${yelpResult}`;
+    convo.say('test');
+    convo.say(yelpResponse);
+    // convo.say('Here\'s what I found' + yelp.search({ term: response.text, location: 'Chicago' }));
+      // convo.next();
+  });
+}
 
-      .then((data) => {
-        if (data.businesses.length != 0) {
-          data.businesses.forEach(business => {
-            convo.say(`${business.name}`);
-            // convo.next();
-          });
-        } else {
-          convo.say('I couldn\'t find any results sorry');
-        }
-      })
-      .catch((err) => {
-        convo.say('Hmm that didn\'t work forEach some reason');
-        convo.next();
-        console.error(err);
-      });
-      // searchYelp(food, location, convo);
-      convo.next();
+controller.hears(['poop'], ['direct_message', 'direct_mention', 'mention', 'message_received'], (bot, message) => {
+  bot.startConversation(message, (err, convo) => {
+    convo.ask('Oh are you hungry? What kind of food would you like?', (response) => {
+      const foodResult = foodSearch(response, convo);
+      convo.say(foodResult);
     });
-  }
+  });
+});
 
 // Using attachments
 // ATTACHMENT source: https://github.com/howdyai/botkit/blob/master/examples/demo_bot.js
 controller.hears(['color', 'colorpicker', 'find color'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
   const attachment = {
-    'username': 'My bot',
-    'text': 'Looking for a quick link to a colorpicker? Here you go :)',
-    'attachments': [
+    username: 'My bot',
+    text: 'Looking for a quick link to a colorpicker? Here you go :)',
+    attachments: [
       {
-        'fallback': 'To be useful, I need you to invite me in a channel.',
-        'title': 'How can I help you?',
-        'text': 'This is my favorite color #ff7575, and my favorite color-picker website: http://htmlcolorcodes.com/color-picker/',
-        'color': '#ff7575',
+        fallback: 'To be useful, I need you to invite me in a channel.',
+        title: 'How can I help you?',
+        text: 'This is my favorite color #ff7575, and my favorite color-picker website: http://htmlcolorcodes.com/color-picker/',
+        color: '#ff7575',
       },
     ],
-    'icon_url': 'https://color.adobe.com/build2.0.0-buildNo/resource/img/kuler/color_wheel_730.png',
+    icon_url: 'https://color.adobe.com/build2.0.0-buildNo/resource/img/kuler/color_wheel_730.png',
   };
 
   bot.reply(message, attachment);
 });
+
+controller.hears(['pizzatime'], ['direct_message', 'direct_mention', 'mention', 'message_received'], (bot, message) => {
+  function askFlavor(response, convo) {
+    convo.ask('What type of food do you want?', (food) => {
+      convo.say('Awesome.');
+      askWhereDeliver(food, convo);
+      convo.next();
+    });
+  }
+  function askWhereDeliver(food, convo) {
+    convo.ask('Where are you looking for food?', (where) => {
+      convo.say(`Ok! So you want ${food.text} near ${where.text}.`);
+      const yelpResult = searchYelp(food.text, where.text, convo);
+      const yelpResponse = 'Here are the two highest rated results near you!';
+      convo.say(yelpResponse);
+    });
+  }
+
+  bot.startConversation(message, askFlavor);
+});
+
 
 // to wake up luisa-bot
 controller.on('outgoing_webhook', (bot, message) => {
@@ -147,7 +153,7 @@ controller.hears(['art', 'explore', 'discover'], ['direct_message', 'direct_ment
         },
       },
       {
-        pattern: bot.utterances.yes,
+        pattern: 'yes',
         callback: () => {
           convo.say('This isn\'t classic art or master works but a great place for people to share what they\'re working on these days: https://www.behance.net/');
           convo.say('or https://dribbble.com/');
@@ -155,7 +161,7 @@ controller.hears(['art', 'explore', 'discover'], ['direct_message', 'direct_ment
         },
       },
       {
-        pattern: bot.utterances.no,
+        pattern: 'no',
         callback: () => {
           convo.say(':\'( Perhaps later...');
           convo.next();
@@ -178,8 +184,7 @@ controller.hears('help', ['direct_message', 'direct_mention', 'mention'], (bot, 
   bot.reply(message, 'As of now if you tell me you\'re hungry or want food I can help you find options near you, also if you need a color picker website I can point you to my favorite one, just mention the word "color", and if you want to explore some cool web developers or illustrators check out Behance or Dribbble and prompt me with "explore" or "art" and I\'ll send you my favorite painting');
 });
 
-// Default message; should go last so it doesn't always say this
+// // Default message; should go last so it doesn't always say this
 controller.hears('', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-  bot.reply(message, 'What are you even talking about');
-  bot.reply(message, 'Fine... If you need help type help');
+  bot.reply(message, 'What are you even talking about... If you need help type "help"');
 });
